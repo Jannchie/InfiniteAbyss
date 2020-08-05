fde818b10b292bd41ab8c198ea8a2472373d2df1
jannchie <jannchie@gmail.com>
2020-08-05 23:21:26 +0900

    Add: Logger

---
+++ Core/Utils/Logger.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace InfiniteAbyss.Core.Utils
+{
+    public static class Logger
+    {
+        public static void Info(string Object, string Msg)
+        {
+            TestContext.Progress.WriteLine($"[ {DateTime.Now} ] {Object} : {Msg}");
+        }
+    }
+}
