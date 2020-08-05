f83428b25a4741f6a6b7f320baad0b7401658763
jannchie <jannchie@gmail.com>
2020-08-05 23:22:37 +0900

    Add: Name

---
+++ Core/Resources/NameData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfiniteAbyss.Core.Resources
+{
+
+    public class NameList {
+        public IList<string> FirstName { get; set; }
+        public IList<string> LastName { get; set; }
+    }
+
+
+    public class NameData
+    {
+        public NameList China { get; set; }
+    }
+}
